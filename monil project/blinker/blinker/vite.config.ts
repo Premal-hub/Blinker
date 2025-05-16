af09b3aa991afdf005a3393097d1916d46e6ffd9
Premal <152951733+Premal-hub@users.noreply.github.com>
2025-05-16 18:21:01 +0530

    Update vite.config.ts

--- monil project/blinker/blinker/vite.config.ts
+++ monil project/blinker/blinker/vite.config.ts
@@ -1,13 +1,7 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/blinker/', // ✅ Replace with your repo name
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
